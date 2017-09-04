16615d211f4381189977784ca1f87e660d7019b2
ritorrehuelamo <torrehuelamo@gmail.com>
2017-09-04 23:41:02 +0200

    Done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,72 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0
+    this.MAXFLOOR = 10
+    this.direction = 'Stand'
+    this.requests = []
+    this.waitingList = []
+    this.passengers = []
+		this.interval
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.interval = setInterval(() => this.update(), 1000)
+  }
+
+  stop() {
+    clearInterval(this.interval)
+  }
+
+  update() {
+		this.log()
+		if (this.requests.length == 0) this.stop()
+		else (this.requests[0] == this.floor) ? this._compareAndLeave() : (this.requests[0] > this.floor) ? this.floorUp() : this.floorDown()
+  }
+
+	_compareAndLeave () {
+		this.requests.shift()
+		this._passengersLeave()
+		this._passengersEnter()
+	}
+
+  _passengersEnter() {
+    this.direction = 'Up'
+    this.waitingList.forEach(person => {
+      if (person.originFloor == this.floor) {
+        this.passengers.push(person)
+        this.requests.push(person.destinationFloor)
+        console.log(`${person.name} has enter the elevator`)
+      }
+    })
+    this.waitingList = this.waitingList.filter(person => person.destinationFloor != this.floor)
+  }
+
+  _passengersLeave() {
+    this.direction = 'Down'
+    this.passengers.forEach(person => {
+			person.destinationFloor == this.floor ? console.log(`${person.name} has left the elevator`) : false
+    })
+    this.passengers = this.passengers.filter(person => person.destinationFloor != this.floor) 
+  }
+
+  floorUp() {
+    this.direction = 'Up'
+    this.floor < this.MAXFLOOR ? this.floor++ : console.log('You can not go ip')
+  }
+
+  floorDown() {
+    this.direction = 'Down'
+    this.floor > 0 ? this.floor-- : console.log('You can not go down')
+  }
+
+  call(person) {
+    this.requests.push(person.originFloor)
+    this.waitingList.push(person)
+  }
+
+  log() {
+    console.log(`Direction: ${this.direction} | Floor: ${this.floor}`)
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,16 @@
-const Elevator = require('./elevator.js');
+const Elevator = require('./elevator.js')
+const Person = require('./person.js')
+
+let elevator = new Elevator()
+let peter = new Person('Peter', 3, 7)
+let aaron = new Person('Aaron', 2, 10)
+let louis = new Person('Louis', 5, 1)
+let arthur = new Person('Arthur', 4, 9)
+
+elevator.start()
+
+elevator.call(peter)
+
+setTimeout(() => elevator.call(aaron), 5000)
+setTimeout(() => elevator.call(louis), 3000)
+setTimeout(() => elevator.call(arthur), 6000)
